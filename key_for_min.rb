46d163ebd1db91ca0a2baca4a21e90cc27cc02b3
David Foote <david.a.foote@gmail.com>
2019-06-07 22:00:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,11 @@ def key_for_min_value(name_hash)
   else
     min_key = name_hash[0]
     min_value = name_hash[1]
-    
-
+    name_hash.each do |key, value|
+      if min_value < value
+        min_value = value
+        min_key = key
+      end
+    end
+      min_key
 end
